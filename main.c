19848d112bee2fc227a5014f8ba1bb33b7ce69be
alvarsnow <alvar@DESKTOP-G5HKSAH.localdomain>
2020-04-25 23:24:29 +0200

    parte 4

--- main.c
+++ main.c
@@ -21,6 +21,9 @@
 #define INFERIOR 0
 #define INTRO 10
 
+#define SUPERIORINT 10000
+#define SUPERIORPORCIENTO 100
+
 #define SUPERIORPART 21
 #define INFERIORPART 1
 
@@ -28,6 +31,11 @@
 
 #define MAXPARTIDO 10
 #define NPARTIDOS 21
+
+#define LETRASTERRITORIO 20
+#define LETRASPARTIDO 11
+
+#define ARCHIVO "Elecciones.csv"
 
 /*
  * Prototipado de funciones
@@ -48,9 +56,12 @@ int limpiarBuffer();
 int main(void) {
   int opt;
 
-  opt = solicitarOpcionMenu();
-  seleccionarOpcion(opt);
-
+  do {
+    opt = solicitarOpcionMenu();
+    seleccionarOpcion(opt);
+    printf("[Pulsa INTRO para continuar]\n");
+    limpiarBuffer();
+  } while (opt != 0);
   // system("clear");
 
   return 0;
@@ -84,7 +95,7 @@ int solicitarOpcionMenu() {
     if (!validarEntero(opt, SUPERIOR, INFERIOR, params, getchar())) {
       printf("! Valor incorrecto, introduce un valor entre %i y %i\n", INFERIOR,
              SUPERIOR);
-      limpiarBuffer();
+
     } else
       return opt;
   }
@@ -102,22 +113,25 @@ void seleccionarOpcion(int opcion) {
   FILE *f;
   switch (opcion) {
     case 2:
-      f = fopen("elecciones.csv", "r");
+      f = fopen(ARCHIVO, "r");
       printf("La fila mas larga es la: %i \n", filaMasLarga(f));
 
       fclose(f);
       break;
     case 1:
-      f = fopen("elecciones.csv", "r");
+      f = fopen(ARCHIVO, "r");
       printf("Hay %i caracteres \n", contarCaracteres(f));
       fclose(f);
       break;
     case 3:
-      f = fopen("elecciones.csv", "r");
+      f = fopen(ARCHIVO, "r");
       resultadosPartido(f);
       fclose(f);
       break;
     case 4:
+      f = fopen(ARCHIVO, "rw");
+      insertarFilas(f);
+      fclose(f);
       break;
     case 0:
       break;
@@ -126,7 +140,7 @@ void seleccionarOpcion(int opcion) {
 }
 
 /*
- * Valida un numerod del menu
+ * Valida un numero del menu
  * @param numero leido
  * @param limite superior
  * @param limite inferior
@@ -143,14 +157,29 @@ bool validarEntero(int num, int sup, int inf, int params, char intro) {
 }
 
 /*
+ * Valida un numero decimal (float), para evitar el redondeo de float a int
+ * @param numero leido
+ * @param limite superior
+ * @param limite inferior
+ * @param parametros leidos por el scanf
+ * @param valor del intro en el scanf
+ * @return si el valor es valido o no
+ */
+bool validarDecimal(float num, int sup, int inf, int params, char intro) {
+  if (num > sup || num < inf || params < 1 || intro != (char)INTRO) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/*
  * Limpiarbuffer
  * @return
  */
 int limpiarBuffer() {
   // clean_stdin();
-  while (getchar() != '\n' || getchar() != EOF)
-    ;
-  return 1;
+  while (getchar() != '\n') return 1;
 }
 
 /*
@@ -238,38 +267,130 @@ void resultadosPartido(FILE *f) {
       "R.I.R", "PCTP/MRPP", "A",    "L",      "IL",      "JPP", "NC",
       "PDR",   "PNR",       "PURP", "PPM",    "MPT",     "PTP", "MAS"};
   do {
-  do {
-    printf("Introduce un partido: \n");
+    do {
+      printf("Introduce un partido: \n");
 
-    for (int i = 0; i < NPARTIDOS; i++) {
-      printf("%i.- %s\n", (i + 1), PARTIDOS[i]);
+      for (int i = 0; i < NPARTIDOS; i++) {
+        printf("%i.- %s\n", (i + 1), PARTIDOS[i]);
+      }
+      printf("0.- Salir\n");
+
+      printf("> ");
+      params = scanf("%i", &opt);
+
+    } while (
+        !validarEntero(opt, SUPERIORPART, INFERIORPART, params, getchar()) &&
+        opt != 0);
+
+    if (opt != 0) {
+      printf(
+          "Instante\tTerritorio\t\tBlancos\tSubscritos\tElegidos\tPorcentaje\tV"
+          "al"
+          "id"
+          "os\t\t"
+          "Voto"
+          "s\tD'Hont\tEstimados\n");
+
+      rewind(f);
+
+      while (fscanf(f, "%i,%20[^,],%i,%i,%i,%11[^,],%i,%f,%f,%i,%i,%i",
+                    &instante, territorio, &blancos, &subs, &elegidos, partido,
+                    &desc, &porcentaje, &validos, &votos, &hondt,
+                    &estimados) != EOF) {
+        if (strcmp(partido, PARTIDOS[opt - 1]) == 0) {
+          printf("%i\t\t%s\t%i\t%i\t\t%i\t\t%f\t%f\t%i\t%i\t%i\n", instante,
+                 territorio, blancos, subs, elegidos, porcentaje, validos,
+                 votos, hondt, estimados);
+        }
+      }
+
+      printf("[Pulsa INTRO para continuar]\n");
+      limpiarBuffer();
     }
-    printf("0.- Salir");
-
-    printf("> ");
-    params = scanf("%i", &opt);
-
-  } while (!validarEntero(opt, SUPERIORPART, INFERIORPART, params, getchar()) && opt != 0);
-
-  printf(
-      "Instante\tTerritorio\t\tBlancos\tSubscritos\tElegidos\tPorcentaje\tValid"
-      "os\t\t"
-      "Voto"
-      "s\tD'Hont\tEstimados\n");
-
-  rewind(f);
-
-  while (fscanf(f, "%i,%20[^,],%i,%i,%i,%11[^,],%i,%f,%f,%i,%i,%i", &instante,
-                territorio, &blancos, &subs, &elegidos, partido, &desc,
-                &porcentaje, &validos, &votos, &hondt, &estimados) != EOF) {
-    if (strcmp(partido, PARTIDOS[opt - 1]) == 0) {
-      printf("%i\t\t%s\t%i\t%i\t\t%i\t\t%f\t%f\t%i\t%i\t%i\n", instante,
-             territorio, blancos, subs, elegidos, porcentaje, validos, votos,
-             hondt, estimados);
-    }
-  }
-
   } while (opt != 0);
-  
-  
+}
+
+/*
+ * Inserta filas en el archivo para el instante elegifo
+ * @param
+ * @return
+ */
+int insertarFilas(FILE *f) {
+  int filas, params, blancos, nulos, subs, instante, total, hondt, objetivo, selecPartido, selecTerri;
+  char opt;
+  char territorio[LETRASTERRITORIO], partido[LETRASTERRITORIO];
+  float elegidos, validos, votos;
+
+  int incFilas = 0;
+
+  do {
+    filas = contarFilas(f);
+    printf("-Se han cargado %i filas-\n", filas);
+
+    printf("Introduce los siguientes datos: \n");
+    printf("Instante de voto (0-3)[0]: ");
+    params = scanf("%i", &instante);
+
+    printf("Territorio:");
+    // leer territorios
+    do {
+      printf("Votos BLANCOS (0-10000): ");
+      params = scanf("%i", &blancos);
+    } while (!validarEntero(blancos, SUPERIORINT, INFERIOR, params, getchar()));
+
+    do {
+      printf("Votos NULOS (0-10000): ");
+      params = scanf("%i", &nulos);
+    } while (!validarEntero(nulos, SUPERIORINT, INFERIOR, params, getchar()));
+
+    do {
+      printf("Subscritos (0-10000): ");
+      params = scanf("%i", &subs);
+    } while (!validarEntero(subs, SUPERIORINT, INFERIOR, params, getchar()));
+
+    printf("Partido: ");
+    // leer partidos
+    do {
+      printf("Elegidos (porcentaje con decimales) (0-100): ");
+      params = scanf("%f", &elegidos);
+    } while (!validarDecimal(subs, SUPERIORPORCIENTO, INFERIOR, params, getchar()));
+
+    do {
+      printf("Porcenataje de votos (0-100): ");
+      params = scanf("%f", &votos);
+    } while (!validarDecimal(votos, SUPERIORPORCIENTO, INFERIOR, params, getchar()));
+
+    do {
+      printf("Porcentaje de votos validos (porcentaje con decimales) (0-100): ");
+      params = scanf("%f", &validos);
+    } while (!validarDecimal(validos, SUPERIORPORCIENTO, INFERIOR, params, getchar()));
+
+    do {
+      printf("Total de votos validos (0-10000): ");
+      params = scanf("%i", &total);
+    } while (!validarEntero(total, SUPERIORINT, INFERIOR, params, getchar()));
+
+    do {
+      printf("Elegidos (Hondt) (0-100): ");
+      params = scanf("%i", &hondt);
+    } while (!validarEntero(hondt, SUPERIORPORCIENTO, INFERIOR, params, getchar()));
+
+    do {
+      printf("Objetivo de elegidos (0-100): ");
+      params = scanf("%i", &objetivo);
+    } while (!validarEntero(objetivo, SUPERIORPORCIENTO, INFERIOR, params, getchar()));
+
+        fprintf("%i,%20[^,],%i,%i,%i,%11[^,],%i,%f,%f,%i,%i,%i", instante,
+                territorio[selecTerri], blancos, nulos, subs, partido[selecPartico], elegidos, validos,
+                votos, total, hondt, objetivo);
+
+    printf("¿Introducir otra fila? (s/n)[N]: ");
+    params = scanf("%c", &opt);
+
+    if (opt == 's' || opt == 'S') filas++;
+
+  } while (opt == 's' || opt == 'S');
+
+  printf("Se han añadido %i filas\n", incFilas);
+  return incFilas;
 }
